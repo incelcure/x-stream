bc2f14b792ace7f769b43a187f260aba3d9ed571
harpoon <sergeekoilia99@gmail.com>
2025-05-11 12:53:26 +0300

    feat: kafka producer

---
+++ src/main/scala/modules/KafkaProducer.scala
@@ -0,0 +1,24 @@
+package modules
+
+import cats.effect.Async
+import domain.XPost
+import fs2.kafka._
+import cats.implicits._
+
+trait KafkaProducer[F[_]] { def send(post: XPost): F[Unit] }
+
+final class KafkaProducerImpl[F[_]: Async](
+                                            cfg: ProducerSettings[F, String, String]
+                                          ) extends KafkaProducer[F] {
+
+  private val producerR = KafkaProducer.resource(cfg)
+
+  override def send(post: XPost): F[Unit] = {
+    producerR.use { producer =>
+      for {
+        fMeta <- producer.produceOne(ProducerRecord("x-stream-data", null, post.text))
+        _ <- fMeta.void
+      } yield ()
+    }
+  }
+}
